1f61baf13bb6fa233c3b05d15e8bff7e3ed5b087
Roger <rgd.afonso@gmail.com>
2022-02-07 21:51:17 -0300

    initial commit

---
+++ Jogo Caça-Frutas/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Jogo_Caça_Frutas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.boxW = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(36, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(651, 412);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.PictureBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.Button1);
+            this.tabPage1.Controls.Add(this.PictureBox2);
+            this.tabPage1.Controls.Add(this.PictureBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(643, 386);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Jogar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Gravar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Digite seu nome:";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Visible = false;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = global::Jogo_Caça_Frutas.Properties.Resources.question;
+            this.PictureBox1.Location = new System.Drawing.Point(31, 51);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(140, 133);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox1.TabIndex = 4;
+            this.PictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(405, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 68);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Jogar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(31, 212);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(191, 68);
+            this.Button1.TabIndex = 7;
+            this.Button1.Text = "Iniciar";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = global::Jogo_Caça_Frutas.Properties.Resources.question;
+            this.PictureBox2.Location = new System.Drawing.Point(247, 51);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(140, 133);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 5;
+            this.PictureBox2.TabStop = false;
+            // 
+            // PictureBox3
+            // 
+            this.PictureBox3.Image = global::Jogo_Caça_Frutas.Properties.Resources.question;
+            this.PictureBox3.Location = new System.Drawing.Point(456, 51);
+            this.PictureBox3.Name = "PictureBox3";
+            this.PictureBox3.Size = new System.Drawing.Size(140, 133);
+            this.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox3.TabIndex = 6;
+            this.PictureBox3.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.boxW);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(643, 386);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ganhadores";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // boxW
+            // 
+            this.boxW.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxW.FormattingEnabled = true;
+            this.boxW.ItemHeight = 16;
+            this.boxW.Location = new System.Drawing.Point(31, 21);
+            this.boxW.Name = "boxW";
+            this.boxW.Size = new System.Drawing.Size(143, 324);
+            this.boxW.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 250;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 400;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 447);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Button button2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.PictureBox PictureBox2;
+        internal System.Windows.Forms.PictureBox PictureBox3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox boxW;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+
+    }
+}
+
---
+++ Jogo Caça-Frutas/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Jogo_Caça_Frutas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a;
+        int b;
+        int c;
+
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            
+            a = random.Next(1, 4);
+            b = random.Next(1, 4);
+            c = random.Next(1, 4);
+
+            switch (a)
+            {
+                case 1:
+                    PictureBox1.Image = Properties.Resources.apple;
+                    break;
+                case 2:
+                    PictureBox1.Image = Properties.Resources.banana;
+                    break;
+                case 3:
+                    PictureBox1.Image = Properties.Resources.morango;
+                    break;
+            }
+
+        }
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            switch (b)
+            {
+                case 1:
+                    PictureBox2.Image = Properties.Resources.apple;
+                    break;
+                case 2:
+                    PictureBox2.Image = Properties.Resources.banana;
+                    break;
+                case 3:
+                    PictureBox2.Image = Properties.Resources.morango;
+                    break;
+            }
+        }
+
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            switch (c)
+            {
+                case 1:
+                    PictureBox3.Image = Properties.Resources.apple;
+                    break;
+                case 2:
+                    PictureBox3.Image = Properties.Resources.banana;
+                    break;
+                case 3:
+                    PictureBox3.Image = Properties.Resources.morango;
+                    break;
+            }
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            {
+                timer1.Enabled = false;
+
+                if (a == b && b == c && a == c)
+                {
+                    textBox1.Visible = true;
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    button3.Visible = true;
+                    label1.Text = "Parabéns você ganhou!!";
+
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label1.Text = "Não foi dessa vez, tente denovo";
+
+                }
+            }
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            boxW.Items.Add(textBox1.Text);
+            textBox1.Visible = false;
+            label1.Visible = true;
+            label2.Visible = false;
+            button3.Visible = false;
+            label1.Text = "Parabéns, " + textBox1.Text + " seu nome foi registrado";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
